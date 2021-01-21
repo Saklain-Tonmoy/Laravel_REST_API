9182c11fa6eb97ce883dca851ad04f95bd4f0ab3
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-01-21 18:08:21 +0600

    commit

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,18 @@ Route::apiResource('/class', ClassesController::class);
 Route::apiResource('/message', TestController::class);
 Route::apiResource('/subject', SubjectController::class);
 Route::apiResource('/student', StudentController::class);
+
+Route::group([
+
+    //'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
